792ad3d1e64fadf91f091e5cc7dc41b429dab68e
xueby1 <321222787@qq.com>
2021-03-27 14:42:41 +0800

    add 20210327 example 1.1--1.4

---
+++ C++/20210327/1-4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Please input two number: ";
+    int a = 0;
+    int b = 0;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << "The sum of " << a 
+            << " and " << b 
+            << " is " << a + b << std::endl;
+    std::cout << "The product of " << a
+            << " and " << b
+            << " is " << a * b << std::endl;
+    system("pause");
+    return 0; 
+}
