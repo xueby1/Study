d3a0b00f56fbc8a4a53a55434648a6e03eb95981
xueby1 <321222787@qq.com>
2021-03-28 17:36:40 +0800

    20210328 example

--- C++/20210327/Sales_item.cpp
+++ C++/20210327/Sales_item.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include "Sales_item.h"
 
 int main () {
@@ -25,7 +26,10 @@ int main () {
     system("pause");
     return 0;
     */
-    /************************ 1.22 类,多个相同的ID，分别求和 **********************/
+    /************************ 1.22 类,多个相同的ID，分别求和 Vector实现，还存在问题 **********************/
+    /*
+    using namespace std;
+    
     unsigned int books_num = 0;
     Sales_item book;
     std::vector<Sales_item> v_sales_item;
@@ -35,17 +39,18 @@ int main () {
     std::cout << "Please input books info: " << std::endl;
     for (unsigned int i = 0; i < books_num; ++i) {
         std::cin >> book;
-        if (v_sales_item.size() > 0) {
-            for (unsigned int j = 0; j < v_sales_item.size(); ++j) {
-                if (book.isbn() == v_sales_item[j].isbn()) {
-                    v_sales_item[j] += book;
-                } else {
-                    // 这里如何才能把ID不存在的加进去？？？
-                    break;
-                }
+        for (unsigned int j = 0; (v_sales_item.size() == 0) || j < v_sales_item.size(); ++j) {
+            if (v_sales_item.size() > 0 && (book.isbn() == v_sales_item[j].isbn())) {
+                v_sales_item[j] += book;
+                cout<<"111"<<endl;
+                break;
+            } else {
+                v_sales_item.push_back(book);
+                cout<<"222"<<endl;
+               // std::cout << v_sales_item[j].isbn() << " Sum is: " << v_sales_item[j] << std::endl;
+                // 这里如何才能把ID不存在的加进去？？？
+                break;
             }
-        } else {
-            v_sales_item.push_back(book);
         }
     }
     for (unsigned int i = 0; i < v_sales_item.size(); ++i) {
@@ -54,4 +59,33 @@ int main () {
 
     system("pause");
     return 0;
+    */
+    /************************ 1.22 类,多个相同的ID，分别求和 map实现 **********************/
+    /*
+    unsigned int books_num = 0;
+    Sales_item book;
+    std::map<std::string, Sales_item> m_sales_item;
+    
+    std::cout << "Please input books number: " << std::endl;
+    std::cin >> books_num;
+    std::cout << "Please input books info: " << std::endl;
+
+    std::map<std::string, Sales_item>::iterator iter;
+    for (unsigned int i = 0; i < books_num; ++i) {
+        std::cin >> book;
+        iter =  m_sales_item.find(book.isbn());
+        if(iter != m_sales_item.end()){
+            iter->second += book;
+        }else{
+            m_sales_item.insert(std::map<std::string, Sales_item>::value_type(book.isbn(),book));
+        }        
+    }
+    for (iter = m_sales_item.begin(); iter != m_sales_item.end(); ++iter) {
+        std::cout << iter->first << " Sum is: " << iter->second << std::endl;
+    }
+    system("pause");
+    return 0;
+    */
+    /************************ 1.25 书店程序 **********************/
+
 }
